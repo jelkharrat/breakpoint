be0d7c9cf81f1eaadf496829ffabaf8e83db3fce
Jesse Elkharrat <jessenelk@gmail.com>
2018-04-30 08:42:45 -0400

    S10L142

---
+++ Breakpoint/Services/AuthService.swift
@@ -0,0 +1,41 @@
+//
+//  AuthService.swift
+//  Breakpoint
+//
+//  Created by Nessin Elkharrat on 4/26/18.
+//  Copyright © 2018 practice. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    //singleton - gives us access to it throughout lifecycle of app in any class
+    static let instance =  AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping COMPLETE) {
+        //createUser is a firebase function
+        FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                 userCreationComplete(false,error)
+                return
+            }
+            
+            //many different kinds of providers like facebook, email...
+            let userData = ["provider": user.providerID, "email": user.email]
+            //if no error, create account for the user. Need a dictionary
+            //pulling uid from firebase
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping COMPLETE) {
+        FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+        guard let user = user else {
+            loginComplete(false, error)
+            return
+        }
+            loginComplete(true, nil)
+    }
+}
---
+++ Breakpoint/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Breakpoint
+//
+//  Created by Nessin Elkharrat on 4/26/18.
+//  Copyright © 2018 practice. All rights reserved.
+//
+
+import Foundation
+
+typealias COMPLETE = (_ Success: Bool, _ error:Error?) -> ()
